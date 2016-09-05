235ab284e1907efcbc04aea0d3458bc7e11eedfc
Juan Luis <Juan Luis@DESKTOP-SH0UAF5>
2016-09-05 10:11:52 +0200

    guardando algunos cambios de javamail

--- src/UserInterface/Correo.java
+++ src/UserInterface/Correo.java
@@ -1,9 +1,19 @@
 package UserInterface;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.swing.JButton;
-import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
 public class Correo extends ZonaMensaje {
@@ -41,6 +51,67 @@ public class Correo extends ZonaMensaje {
 		add(volver);
 		
 		responder = new JButton("Responder");
+		responder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// La dirección de envío (to)
+			    String para = "s_vicuk@hotmail.com";
+
+			    // La dirección de la cuenta de envío (from)
+			    String de = "xtremebos@gmail.com";
+
+			    // El servidor (host). En este caso usamos localhost
+			    String host = "localhost";
+
+			    // Obtenemos las propiedades del sistema
+			    Properties propiedades = System.getProperties();
+			    Properties props = new Properties();
+			    props.put("mail.smtp.auth", "true");
+		        props.put("mail.smtp.starttls.enable", "true");
+		        props.put("mail.smtp.host", "smtp.gmail.com");
+		       // props.put("mail.smtp.host", "smtp.live.com");
+		        props.put("mail.smtp.port", "587");
+		       // props.put("mail.smtp.user", de);
+		        
+//		        propiedades.put("mail.smtp.auth", "false");
+//		        propiedades.put("mail.smtp.starttls.enable", "true");
+//		        propiedades.put("mail.smtp.host", "smtp.gmail.com");
+//		        propiedades.put("mail.smtp.port", "587");
+
+			    // Configuramos el servidor de correo
+			    propiedades.setProperty("mail.smtp.host", host);
+
+			    // Obtenemos la sesión por defecto
+			    Session sesion = Session.getDefaultInstance(props);
+			    sesion.setDebug(true);
+
+			    try{
+			      // Creamos un objeto mensaje tipo MimeMessage por defecto.
+			      MimeMessage mensaje = new MimeMessage(sesion);
+
+			      // Asignamos el “de o from” al header del correo.
+			      mensaje.setFrom(new InternetAddress(de));
+
+			      // Asignamos el “para o to” al header del correo.
+			      mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(para));
+
+			      // Asignamos el asunto
+			      mensaje.setSubject("Primer correo sencillo");
+
+			      // Asignamos el mensaje como tal
+			      mensaje.setText("Probando correo con JavaMail");
+
+			      // Enviamos el correo
+			      Transport t = sesion.getTransport("smtp");
+			      //t.connect("juanlu_bf@hotmail.com", "JL_02_06_1995");
+			      t.connect("Xtremebos@gmail.com", "lacontraseniadegogle123");
+			      t.sendMessage(mensaje,mensaje.getAllRecipients());
+			      t.close();
+			      System.out.println("Mensaje enviado");
+			    } catch (MessagingException e) {
+			      e.printStackTrace();
+			    }
+			}
+		});
 		responder.setBounds(254, 479, 131, 23);
 		add(responder);
 	}
